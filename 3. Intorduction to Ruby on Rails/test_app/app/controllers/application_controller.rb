763794f65d5c970df1001e60dcea8eee4b9a2bb0
charandeep.singh <ckaunsel123@gmail.com>
2022-04-14 22:35:47 +0530

    testing git commit

--- 3. Intorduction to Ruby on Rails/test_app/app/controllers/application_controller.rb
+++ 3. Intorduction to Ruby on Rails/test_app/app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
     
     # actions -> methods
     def hello
-        render html: "Hello World!"
+        render html: "Hello World!!!"
     end
 end
