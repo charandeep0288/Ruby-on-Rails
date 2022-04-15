d11d0d793fa8a8be00247a7c52b86d171d75d521
charandeep.singh <ckaunsel123@gmail.com>
2022-04-15 11:20:21 +0530

    added about page to test_app using Ruby on Rails

--- 3. Intorduction to Ruby on Rails/test_app/app/controllers/application_controller.rb
+++ 3. Intorduction to Ruby on Rails/test_app/app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
     
     # actions -> methods
     def hello
-        render html: "Hello World!!!"
+        # render html: "Hello World!!!"
+    end
+
+    def about 
     end
 end
--- 3. Intorduction to Ruby on Rails/test_app/config/routes.rb
+++ 3. Intorduction to Ruby on Rails/test_app/config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
 
   # root 'application#hello' # It is expecting hello function from controller file route 
   root 'pages#home' # home method in pages controller
+
+  # get request to '/about' page, and send this to 'pages' controller and 'about' action(method)
+  get 'about', to: 'pages#about'
 end
